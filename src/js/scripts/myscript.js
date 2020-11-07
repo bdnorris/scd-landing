c43e8f1f9609795b5eac9bc0308382d72489f899
Brian <bdnorris@gmail.com>
2020-11-06 20:38:17 -0600

    install pageclip

--- src/js/scripts/myscript.js
+++ src/js/scripts/myscript.js
@@ -2,11 +2,6 @@ export default () => {
   
   console.log("Hello World!");
 
-  // testing arrow functions
-  document.querySelector('h1').addEventListener('click', (e) => {
-    e.currentTarget.style.color = (e.currentTarget.style.color === 'red') ? 'black' : 'red';
-  })
-
   // testing polyfills with NodeList forEach
   document.querySelectorAll('h2').forEach((d) => {
     console.log(d.innerText);
